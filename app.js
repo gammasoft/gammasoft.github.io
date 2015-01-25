0164b5fd863a142a3cee05f4663f978255c0a07e
Renato Gama <renatoargh@gmail.com>
2015-01-25 19:06:41 -0200

    more testing with heroku

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var http = require('http');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT || 5000, '127.0.0.1');
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Hello World\n');
+}).listen(process.env.PORT || 5000);
