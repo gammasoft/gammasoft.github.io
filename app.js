b157eeb69eee83343456ff85eb1a8a52a4df2677
Renato Gama <renatoargh@gmail.com>
2015-01-25 19:43:17 -0200

    started implementing github access token retrieval

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-var http = require('http');
+var http = require('http'),
+    url = require('url'),
+    request = require('request');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
+http.createServer(function(req, res) {
+    var url_parts = url.parse(req.url, true),
+        query = url_parts.query;
+
+    res.setHeader('Content-Type', 'application/json');
+
+    request({
+        uri: 'https://github.com/login/oauth/access_token',
+        method: 'POST',
+        json: {
+            client_id: process.env.CLIENT_ID,
+            cliente_secret: process.env.CLIENT_SECRET,
+            code: query.code
+        }
+    }, function(err, response, body) {
+        if(err || body.error) {
+            res.statusCode = 500;
+        }
+
+        res.statusCode = 200;
+        res.end(JSON.stringify(body));
     });
-
-    res.end('Hello World\n');
 }).listen(process.env.PORT || 5000);
